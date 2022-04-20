e4d3349a630b41045e8c909b134e919273c0fdef
Fardin <fardinarafat30@gmail.com>
2022-04-20 12:57:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if (process.env.VERCEL !== "1") {
     rs.send("Home [express] " + version);
   });
 
-  var server = app.listen(8080, function () {
+  var server = app.listen(80, function () {
     console.log("Listening on port " + server.address().port);
   });
 
